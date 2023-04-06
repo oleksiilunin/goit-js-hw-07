8fd36f02973b41e238bab73a40a1d4bf8abcec0e
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-04-06 23:32:56 +0300

    Create markup for gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const markupGallery = document.querySelector('.gallery');
+
+(function () {
+	const markup = galleryItems.map(({ preview, original, description }) => `
+	<li class="gallery__item">
+		<a class="gallery__link" href="${original}">
+			<img
+				class="gallery__image"
+				src="${preview}"
+				data-source="${original}"
+				alt="${description}"
+			/>
+		</a>
+	</li>
+	`).join('');
+	markupGallery.insertAdjacentHTML('beforeend', markup)
+})();
